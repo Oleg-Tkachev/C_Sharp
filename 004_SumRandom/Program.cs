cc61c5417ca5619a08b5b644e951db81826f0b86
unknown <ot250013@yandex.ru>
2022-08-18 14:38:43 +0300

    add SumRandom 01

--- 004_SumRandom/Program.cs
+++ 004_SumRandom/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA = new Random().Next(1, 11); // случайное число от 1 до 10 
+int numberB = new Random().Next(1, 11);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
