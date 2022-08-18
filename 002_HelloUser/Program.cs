b9b1b1780a98a2933fccdc0921de5eaaecf56bb1
unknown <ot250013@yandex.ru>
2022-08-18 13:43:07 +0300

    add 002 HelloUser

---
+++ 002_HelloUser/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите ваше имя:");
+string username = Console.ReadLine();
+Console.Write("Введите ваш возраст:");
+string userold = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+Console.Write(" тебе, ");
+Console.Write(userold);
+Console.Write(" года");
+
