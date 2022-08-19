287ab53b1e57f1cc2c132c7ee4dbd524976e7ad8
unknown <ot250013@yandex.ru>
2022-08-19 23:46:42 +0300

    add task четное/нечетное

---
+++ HomeWork1/Task number 3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.WriteLine("Please enter nambers");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Введенное число четное");
+}
+else Console.WriteLine("Введенное число нечетное");
