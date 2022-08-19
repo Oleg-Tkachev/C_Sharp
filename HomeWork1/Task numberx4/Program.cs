eb401bfa1abe581d63cf57235e4ee717e918c3c8
unknown <ot250013@yandex.ru>
2022-08-20 00:05:00 +0300

    add task 4 цикл.

---
+++ HomeWork1/Task numberx4/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Please enter namber");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i=1; i<= number; i++) // Программа не работает если ввести число с "-" 
+if (i % 2 == 0)
+{
+   Console.WriteLine(i); 
+}
+
