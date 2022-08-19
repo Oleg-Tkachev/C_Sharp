4364a37489c40a23417cd525c3ee15446d3bac14
unknown <ot250013@yandex.ru>
2022-08-19 23:25:22 +0300

    add task 2

---
+++ HomeWork1/Task number 2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Please enter first number");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Plese enter second number ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter third number");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+int bigNum = numberA;
+
+if (numberB > bigNum)
+    {
+    bigNum = numberB;
+    }
+if (numberC > bigNum)
+    {
+    bigNum = numberC;;
+    }
+Console.WriteLine("Максимальное число из: {0}, {1}, {2}, является {3}", numberA, numberB, numberC, bigNum);
