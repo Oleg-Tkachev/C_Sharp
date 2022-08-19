c516b868e954e5a6932de88e4628417856dbb624
unknown <ot250013@yandex.ru>
2022-08-19 19:44:49 +0300

    Task number 1

---
+++ HomeWork1/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.WriteLine("Please enter first number");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please enter second number");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if (numberA > numberB)
+    {
+        Console.Write("Максимальное число: ");
+        Console.Write(numberA);
+    }
+else 
+    {
+        Console.Write("Максимальное число: ");
+        Console.Write(numberB);
+    }
