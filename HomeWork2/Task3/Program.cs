693b2e93cfa3aae37f8ce8aaee3bd266dc48475c
unknown <ot250013@yandex.ru>
2022-08-24 00:48:38 +0300

    add task 3 OK

---
+++ HomeWork2/Task3/Program.cs
@@ -0,0 +1,40 @@
+﻿
+void DayWeek(int day)
+{
+    switch (day)
+    {
+        case (1):
+            Console.WriteLine("Понедельник");
+            break;
+        case (2):
+            Console.WriteLine("Вторник");
+            break;
+        case (3):
+            Console.WriteLine("Среда");
+            break;
+        case (4):
+            Console.WriteLine("Четверг");
+            break;
+        case (5):
+            Console.WriteLine("Пятница");
+            break;
+        case (6):
+            Console.WriteLine("Суббота - ВЫХОДНОЙ!");
+            break;
+        case (7):
+            Console.WriteLine("Воскресенье - ВЫХОДНОЙ!");
+            break;
+    }
+}
+
+Console.WriteLine("Введите цифру, согласно дню недели: 1 - Понедельник");
+Console.WriteLine("Введите цифру, согласно дню недели: 2 - Вторник");
+Console.WriteLine("Введите цифру, согласно дню недели: 3 - Среда");
+Console.WriteLine("Введите цифру, согласно дню недели: 4 - Четверг");
+Console.WriteLine("Введите цифру, согласно дню недели: 5 - Пятница");
+Console.WriteLine("Введите цифру, согласно дню недели: 6 - Суббота");
+Console.WriteLine("Введите цифру, согласно дню недели: 7 - Воскресенье");
+
+int day = Convert.ToInt32(Console.ReadLine());
+DayWeek(day);
+
