ec1de09f4bf0aefbc1e052475e955b63cf209087
unknown <ot250013@yandex.ru>
2022-08-24 01:20:24 +0300

    add task1 OK

--- HomeWork2/Task1/Program.cs
+++ HomeWork2/Task1/Program.cs
@@ -1,7 +1,11 @@
 ﻿
+void SecondDig(int number)
+{
+    int num = (number / 10) % 10;
+    Console.WriteLine(num);
+}
+
+
 Console.WriteLine("Please enret number 100 - 999");
 int number = Convert.ToInt32(Console.ReadLine());
-
-int num = (number / 10) % 10;
-Console.WriteLine(num);
-
+SecondDig(number);
