285cf229629e103a7bea9d624efea8b82b522c4c
unknown <ot250013@yandex.ru>
2022-08-26 15:57:04 +0300

    Add zoom files

---
+++ Zoom_vebinar/Seminar002/Program.cs
@@ -0,0 +1,123 @@
+﻿
+/* Программа, которая выводит случайное число из отрезка
+[10, 99] и показывает набольшую цифру числа
+78 => 8
+12 => 2
+87 => 8
+*/
+
+Random random = new Random();           //генерация случайного числа
+int randomNum = random.Next(10, 100);   // предел рандомных чисел 10 - 99
+int digi1 = randomNum / 10;             // получаем первую цифру 
+int digi2 = randomNum % 10;             // получаем вторую цифру 
+Console.WriteLine($"Рандомное число: {randomNum}");
+if (digi1 > digi2)
+    {
+    Console.WriteLine($"Наибольшая цифра {digi1}");
+    }
+else 
+    {
+    Console.WriteLine($"Наибольшая цифра {digi2}"); //интерполяция
+    }
+
+//////////////////// Через метод!  ////////////////////////////////
+
+Random random = new Random();           
+int randomNum = random.Next(10, 100);   
+Console.WriteLine($"Рандомное число: {randomNum}");
+
+int GetMaxNumber (int number)
+{
+    int digi1 = randomNum / 10;
+    int digi2 = randomNum % 10;
+if (digi1 > digi2)
+    {
+    return digi1;
+    }
+else 
+    {
+    return digi2;
+    }
+}
+
+int result = GetMaxNumber(randomNum);
+Console.WriteLine($"Наибольшая цифра в числе: {result}");
+
+
+
+
+///////////////////////////////////////////////////////////////////////////
+/* Программа, которая выводит случайное трехзначное число и 
+удаляет вторую цифру этого числа.
+456 => 46
+782 => 72
+*/
+
+Random random = new Random();
+int randomNumber = random.Next(100, 1000);
+
+int secondDigi = (randomNumber / 10)%10;
+Console.WriteLine($"Случайное число: {randomNumber}");
+Console.WriteLine($"Вторая цифра: {secondDigi}");
+
+
+
+///////////////////////////////////////////////////////////////////////////
+/* Программа, которая на вход принимает два числа и выводит
+является ли второе число кратным первому. Если, число 2 не кратно числу 1,
+то программа выводит остаток от деления. 
+34, 5 => не кратно, остаток 4
+16, 4 => кратно 
+*/
+
+Console.WriteLine("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1 % num2;
+if (num1 % num2 == 0)
+    {
+    Console.WriteLine("Кратно");
+    }
+else
+    {
+    Console.WriteLine($"Не кратно, остаток {result}");
+    }
+
+
+///////////////////////////////////////////////////////////////////////////
+
+/* Программа, которая проверяет введенное число на кратность двум
+введенным числам из консоли. Выводит результат. 
+55 => 5, 11 = кратно 
+10 => 2, 5 = кратно
+44 => 4, 10 = не кратно 
+*/
+
+// Решение через метод с вводом трех данных (number, firstDivide, secondDivide)
+///////////////////////////////////////////////////////////////////////////////
+
+
+bool CheckNumber(int number, int firstDivide, int secondDivide)
+{
+    if (number % firstDivide == 0 && number % secondDivide == 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Введите число для проверки: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите первый делитель: ");
+int firstDivide = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второй делитель: ");
+int secondDivide = Convert.ToInt32(Console.ReadLine());
+
+bool check = CheckNumber(number, firstDivide, secondDivide);
+Console.WriteLine(check);
+ 
+////////////////////////////////////////////////////////////////////////////
---
+++ Zoom_vebinar/Seminar003/Program.cs
@@ -0,0 +1,143 @@
+﻿
+
+
+
+// Console.WriteLine("Введите x");
+// bool isNumberX = int.TryParse(Console.ReadLine(), out int x);
+
+// Console.WriteLine("Введите y");
+// bool isNumberY= int.TryParse(Console.ReadLine(), out int y);
+
+// if(isNumberX != true || isNumberY != true)
+// {
+//     Console.WriteLine("Числа введены не верно.");
+//     return;
+// }
+
+// bool isValid = Validate(x,y);
+// if(isValid == false)
+// {
+//  Console.WriteLine("Како-то число равно нулю");
+//  return;
+// }
+
+// int result = GetPosition(x,y);
+// Console.WriteLine(result);
+
+// bool Validate(int x, int y)
+// {
+//     if(x == 0 || y == 0)
+//     {
+//         return false;
+//     }
+//     return true;
+// }
+
+// int GetPosition(int x, int y)
+// {
+//     if(x>0 && y>0)
+//     {
+//         return 1;
+//     }
+
+//     if(x < 0 && y > 0)
+//     {
+//         return 2;
+//     }
+
+//     if(x < 0 && y < 0)
+//     {
+//         return 3;
+//     }
+
+//     if(x > 0 && y < 0)
+//     {
+//         return 4;
+//     }
+
+//     return -1;
+// }
+
+
+// Console.WriteLine("Введите номер четверти");
+// int position = Convert.ToInt32(Console.ReadLine());
+
+
+// Console.WriteLine(GetPositio(position));
+
+// string GetPositio(int position)
+
+// {
+//     if (position == 1)
+//     {
+
+//         return "x > 0, y > 0";
+//     }
+
+//     if (position == 2)
+//     {
+//         return "x > 0, y < 0";
+//     }
+//     if (position == 3)
+//     {
+//         return "x < 0, y < 0";
+//     }
+//     if (position == 4)
+//     {
+//         return "x > 0, y < 0";
+//     }
+//     return " Неверная цифра";
+// }
+
+
+
+
+System.Console.WriteLine("Введите координату х первой точки: ");
+bool isNumberX_1 = int.TryParse(Console.ReadLine(),out int x1);
+
+System.Console.WriteLine("Введите координату у первой точки: ");
+bool isNumberY_1 = int.TryParse(Console.ReadLine(),out int y1);
+
+System.Console.WriteLine("Введите координату х второй точки: ");
+bool isNumberX_2 = int.TryParse(Console.ReadLine(),out int x2);
+
+System.Console.WriteLine("Введите координату у второй точки: ");
+bool isNumberY_2 = int.TryParse(Console.ReadLine(),out int y2);
+
+if (!isNumberX_1|| !isNumberX_2 || !isNumberY_1 || !isNumberY_2) 
+{
+    Console.WriteLine("Числа введены неверно");
+    return;
+}
+System.Console.WriteLine($"расстояние: {GetLenght(x1, y1, x2, y2)}");
+double GetLenght(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt(Math.Pow((x2 -x1), 2) + Math.Pow((x2 - y2), 2));
+}
+
+
+Console.WriteLine("\n Задача 23");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
