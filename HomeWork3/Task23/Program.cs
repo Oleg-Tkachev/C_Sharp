5c682c5d500766d7a29b6ef8ba29daf88283a84f
unknown <ot250013@yandex.ru>
2022-08-29 00:10:21 +0300

    add task 23 OK

--- HomeWork3/Task23/Program.cs
+++ HomeWork3/Task23/Program.cs
@@ -9,17 +9,16 @@
 Console.WriteLine("please enter number");
 bool isNumber = int.TryParse(Console.ReadLine(), out int num);  // ввод данных + проверка на цифровой ввод
 
-
 if (isNumber != true || num <= 0)
 {
     Console.WriteLine("Некорректный ввод.");
-    return;                                              // return, если введен символ и отрицательное число
+    return;                                             // return, если введен символ и отрицательное число
 }
 
-int[] GetExponent(int num)                               // метод, получаем куб числа.
+int[] GetExponent(int num)                              // метод, получаем куб числа.
 {
     int[] array = new int[num];
-    for (int i = 1; i <= num; i++)                       // реализовал чере цикл for while был на семинаре, эксперемент)
+    for (int i = 1; i <= num; i++)                      // реализовал чере цикл for while был на семинаре, эксперемент)
     {
         array[i - 1] = (int) Math.Pow(i, 3);
     }
@@ -28,7 +27,7 @@ int[] GetExponent(int num)                               // метод, полу
 
 void PrintArray(int[] exponent)                         
 {
-    for (int i = 0; i < exponent.Length; i++)           // реализовал чере цикл for while был на семинаре, эксперемент)
+    for (int i = 0; i < exponent.Length; i++)          // реализовал чере цикл for while был на семинаре, эксперемент)
     {
         Console.Write($" {exponent[i]}");
     }
