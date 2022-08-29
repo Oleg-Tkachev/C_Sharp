27815bf7fdf6d112bb1aa36e6a52322e46f6d131
unknown <ot250013@yandex.ru>
2022-08-30 02:10:46 +0300

    task 25 in progress

--- HomeWork4/Task25/Program.cs
+++ HomeWork4/Task25/Program.cs
@@ -6,3 +6,27 @@
 2, 4 ==> 16
 */
 
+
+Console.WriteLine("Введите число A:");
+double numberA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите число B (степень):");
+double numberB = Convert.ToDouble(Console.ReadLine());
+if (numberB == 0)
+{
+    Console.WriteLine("Любое число в степени ^0 = 1");
+    return;
+}
+
+double GetExpondouble(double numberA, double numberB)
+{
+  double result = 1;
+  for(double i=1; i <= numberB; i++)
+  {
+    result = result * numberA;
+  }
+    return result;
+}
+
+double result = GetExpondouble(numberA, numberB);
+Console.WriteLine($"Результат: {result}");
