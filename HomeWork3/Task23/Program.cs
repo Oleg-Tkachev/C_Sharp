304a8fdaa439fd3ecddc8970ff5dd3cbf45a3c4b
unknown <ot250013@yandex.ru>
2022-08-29 17:20:11 +0300

    add new solution

--- HomeWork3/Task23/Program.cs
+++ HomeWork3/Task23/Program.cs
@@ -34,3 +34,57 @@ void PrintArray(int[] exponent)
 }
 
 PrintArray(GetExponent(num));
+
+
+
+/* 
+Программа, которая принимает на вход число (N) 
+и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+/////////////////////////////// Второй вариант \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+using static System.Console;
+int N = InputNumber("Введите целое число: ");
+if (N != 0)
+{
+    int n = N < 0 ? Math.Abs(N) + 2 : N;
+    int[] arrey = new int[n];
+    arrey = GetCub(N, n);
+    WriteArrey("Кубы от 1 до "+Convert.ToString(N)+": ", arrey, n);
+}
+else { Write(N); }
+WriteLine();
+
+void WriteArrey(string text, int[] arrey, int n)
+{
+    Write(text);
+    Write(arrey[0]);
+    for (int i = 1; i < n; i++)
+    {
+        Write($", {arrey[i]}");
+    }
+}
+
+int InputNumber(string text)
+{
+    bool isNumber = false; int x = 0;
+    while (!isNumber)
+    {
+        Write(text);
+        isNumber = int.TryParse(ReadLine(), out x);
+        if (!isNumber) { WriteLine("Это не число"); }
+    }
+    return x;
+}
+
+int[] GetCub(int N, int n)
+{
+    int[] arrey = new int[n];
+    for (int i = 1; i < n + 1; i++)
+    {
+        arrey[i - 1] = N < 0 ? (int)Math.Pow(2 - i, 3) : i * i * i;
+    }
+    return arrey;
+}
