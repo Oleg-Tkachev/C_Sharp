125a80368964727bf1304eee145625bd8323083b
unknown <ot250013@yandex.ru>
2022-08-30 02:27:07 +0300

    task 27 in progress

--- HomeWork4/Task27/Program.cs
+++ HomeWork4/Task27/Program.cs
@@ -7,4 +7,28 @@
 9021 ==> 12
 */
 
+Console.Write("Plase enter number: ");
+bool isNumber = int.TryParse(Console.ReadLine(), out int number);  // проверка на ввод цифр не буквы
+if (isNumber != true)
+{
+    Console.WriteLine("Некорректный ввод данных");
+    return;
+}
 
+int GetSumDigi(int number)
+{
+    int len = Convert.ToString(number).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        advance = number - number % 10;
+        result = result + (number - advance);
+        number = number / 10;
+    }
+    return result;
+}
+
+int sum = GetSumDigi(number);
+Console.WriteLine($"Сумма цифр в числе: {sum}");
