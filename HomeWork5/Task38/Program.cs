c12c3ffb324b3d3caa6fb239c25ad79aab00e322
unknown <ot250013@yandex.ru>
2022-09-02 23:27:59 +0300

    Add task38 OK

--- HomeWork5/Task38/Program.cs
+++ HomeWork5/Task38/Program.cs
@@ -4,7 +4,6 @@
 [3 7 22 2 78] -> 76
 */
 
-
 Console.WriteLine("Введите длину массива:");
 bool isNumber = int.TryParse(Console.ReadLine(), out int length);  //получаем длину массива
 if (!isNumber || length <= 0)
@@ -13,42 +12,41 @@ if (!isNumber || length <= 0)
     return;
 }
 
-int [] array = RandomArrey(length);              // передаем длину массива lenght в метод Random
+int[] array = RandomArrey(length);              // передаем длину массива lenght в метод Random
 
-
-int [] RandomArrey(int len)                      // метод по генерации рандомных чисел от 100 до 999
+int[] RandomArrey(int len)                      // метод по генерации рандомных чисел от 100 до 999
 {
-    int[] array = new int [length];
-    for (int i = 0; i < length; i++)               // цикл заполняем массив
+    Console.Write("[ ");
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)              // цикл заполняем массив
     {
-        array[i] = new Random().NextDouble(-100 , 101);
+        array[i] = new Random().Next(-100, 101);  // случайные числа от -100 до 100
         Console.Write(array[i] + " ");
     }
-return array;
+    Console.Write("]");
+    return array;
 }
-
 
 int maxNumber = array[0];
 int minNumber = array[0];
 GetMaX_Min_Numbers(array);
 
-void GetMaX_Min_Numbers(int [] number)
+void GetMaX_Min_Numbers(int[] number)      //метод выводит maxNumber, minNumber 
 {
-
-    for (int i = 1; i < array.Length; i++)
+  for (int i = 1; i < array.Length; i++)
     {
       if (maxNumber < array[i])
         {
             maxNumber = array[i];
         }
-          //Console.WriteLine(maxNumber);
       if (minNumber > array[i])
         {
             minNumber = array[i];
         }
-          //Console.WriteLine(minNumber);
     }
 }
 
 int result = maxNumber - minNumber;
-Console.WriteLine($"Разница между между максимальным [{maxNumber}] и минимальным[{minNumber}] элементами: {result}");
+Console.WriteLine();
+Console.WriteLine($"Разница между между максимальным [{maxNumber}] и минимальным [{minNumber}] элементами = [{result}]");
+Console.WriteLine();
