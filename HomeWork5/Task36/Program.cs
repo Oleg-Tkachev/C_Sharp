2cdf226c77d3812b563eea6b70ba5b974b2866c9
unknown <ot250013@yandex.ru>
2022-09-02 19:28:18 +0300

    Add task36 OK

--- HomeWork5/Task36/Program.cs
+++ HomeWork5/Task36/Program.cs
@@ -5,3 +5,41 @@
 [-4, -6, 89, 6] -> 0
 */
 
+Console.WriteLine("Введите длину массива:");
+bool isNumber = int.TryParse(Console.ReadLine(), out int length);  //получаем длину массива
+if (!isNumber || length <= 0)
+{
+    Console.WriteLine("Некорректный ввод");                        //проверяем 
+    return;
+}
+
+int[] array = RandomArrey(length);          // передаем длину массива lenght в метод RandomArrrey
+SumOddNumbers(array);                       //передаем рандомный массив в метод подсчета суммы по нечет.индексам
+
+int[] RandomArrey(int len)                  // метод по генерации рандомных чисел от -99 до 99
+{
+    int[] array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)        // цикл заполняем массив
+    {
+        array[i] = random.Next(-99, 100);     // случайные числа от -99 до 99
+    }
+    return array;
+}
+
+void SumOddNumbers(int[] random)           // метод подсчета чисел нечет.индексов + вывод 
+{
+    Console.Write("[");
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+        Console.Write($" {array[i]}");
+    }
+  Console.Write(" ]");
+  Console.WriteLine();
+  Console.WriteLine($"Cумма элементов, стоящих на нечётных позициях  = {sum}");
+}
