9bdf9c60180d175f30a135029851038d816ea768
unknown <ot250013@yandex.ru>
2022-09-18 23:39:12 +0300

    add Task 66 ok

---
+++ HomeWork9/Task66/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+/*
+Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите значение M: ");
+bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
+
+Console.Write("Введите значение N: ");
+bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isNumberM || !isNumberM || m > n)
+{
+    Console.WriteLine("Некорректный ввод");
+    return;
+}
+
+
+int sum = 0;
+PrintSum(m, n, sum);
+
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {sum}");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+}
--- HomeWork9/Task68/Program.cs
+++ HomeWork9/Task68/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 /*
-Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
 
 m = 2, n = 3 -> A(m,n) = 29
 */
@@ -11,20 +12,29 @@ bool isNumberM = int.TryParse(Console.ReadLine(), out int m);
 Console.Write("Введите значение N: ");
 bool isNumberN = int.TryParse(Console.ReadLine(), out int n);
 
-if (!isNumberM || !isNumberM)
+if (!isNumberM || !isNumberM || m < 0 || n < 0)
 {
     Console.WriteLine("Некорректный ввод");
     return;
 }
 
-int functionAkkerman = Ack(m, n);
 
-Console.Write($"Функция Аккермана = {functionAkkerman} ");
+int functionAkerman = Akerman(m, n);
+Console.Write($"Функция Аккермана = {functionAkerman} ");
 
-int Ack(int m, int n)
+
+int Akerman(int m, int n)
 {
-  if (m == 0) return n + 1;
-  else if (n == 0) return Ack(m - 1, 1);
-  else return Ack(m - 1, Ack(m, n - 1));
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akerman(m - 1, Akerman(m, n - 1)));
+    }
 }
-
